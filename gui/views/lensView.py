b2bf390e7231f8e17b3869f9a66e17a7c7f26cc7
Marc-André <marc-andre.vigneault.02@hotmail.com>
2021-03-16 14:10:56 -0400

    Better requirements + lensView TeardOwn

--- gui/views/lensView.py
+++ gui/views/lensView.py
@@ -1,14 +1,7 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QFileDialog, QMessageBox
-from PyQt5.QtCore import pyqtSlot, pyqtSignal, QModelIndex, Qt, QAbstractItemModel
-from pyqtgraph import PlotItem, BarGraphItem
-from gui.widgets.parametersTableWidget import ParametersTableModel
-from gui.widgets.parametersTableWidget import ParametersTableView
-from PyQt5 import uic, QtCore
-from scipy.stats import binom, geom, dlaplace, logser, nbinom, poisson, planck, randint, zipf
-import numpy as np
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtCore import pyqtSignal
+from PyQt5 import uic
 import logging
-import json
-import time
 import os
 
 log = logging.getLogger(__name__)
